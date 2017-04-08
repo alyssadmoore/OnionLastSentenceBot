6d9bad107fb0763f1cfa6b25d5856bcea96494d4
alyssadmoore <alyssadmoore@gmail.com>
2017-04-07 19:17:21 -0500

    Test image upload

--- bot.js
+++ bot.js
@@ -8,20 +8,42 @@ var Bot = new TwitterBot({
     access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
 });
 
-var phraseArray = [ "hello darkness",
-    "my old friend",
-    "chirp chirp",
-    "so how bout that weather",
-    "indescribable",
-    "delete this neffew",
-    "itz me snek",
-    "meirl",
-    "how would pants wear pants?",
-    "i aint the sharpest tool in the shed" ];
 
-function chooseRandom(myArray) {
-    return myArray[Math.floor(Math.random() * myArray.length)];
+function upload_image(){
+    var image_path = 'https://upload.wikimedia.org/wikipedia/commons/f/f8/Thomisidae_feeding_Junonia_almana_on_Acmella_ciliata-Kadavoor-2015-08-21-001.jpg',
+        b64content = fs.readFileSync(image_path, { encoding: 'base64' });
+
+    Bot.post('media/upload', { media_data: b64content }, function (err, data, response) {
+        if (err){
+            console.log('ERROR:');
+            console.log(err);
+        }
+        else {
+            Bot.post('statuses/update', {
+                    media_ids: new Array(data.media_id_string)
+                }
+            );
+        }
+    });
 }
 
-var phrase = chooseRandom(phraseArray) + ", " + chooseRandom(phraseArray);
-Bot.tweet(phrase);
+upload_image();
+
+
+// var phraseArray = [ "hello darkness",
+//     "my old friend",
+//     "chirp chirp",
+//     "so how bout that weather",
+//     "indescribable",
+//     "delete this neffew",
+//     "itz me snek",
+//     "meirl",
+//     "how would pants wear pants?",
+//     "i aint the sharpest tool in the shed" ];
+//
+// function chooseRandom(myArray) {
+//     return myArray[Math.floor(Math.random() * myArray.length)];
+// }
+//
+// var phrase = chooseRandom(phraseArray) + ", " + chooseRandom(phraseArray);
+// Bot.tweet(phrase);
