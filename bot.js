623c4295b9bec46988c62d31fa5ba2c8c7a42da4
alyssadmoore <alyssadmoore@gmail.com>
2017-04-07 19:28:54 -0500

    Test image upload

--- bot.js
+++ bot.js
@@ -10,40 +10,15 @@ var Bot = new TwitterBot({
 
 
 function upload_image(){
-    var image_path = 'https://upload.wikimedia.org/wikipedia/commons/f/f8/Thomisidae_feeding_Junonia_almana_on_Acmella_ciliata-Kadavoor-2015-08-21-001.jpg',
+    var image_path = path.join(__dirname, '/images/1.jpg'),
         b64content = fs.readFileSync(image_path, { encoding: 'base64' });
 
     Bot.post('media/upload', { media_data: b64content }, function (err, data, response) {
-        if (err){
-            console.log('ERROR:');
-            console.log(err);
-        }
-        else {
-            Bot.post('statuses/update', {
-                    media_ids: new Array(data.media_id_string)
-                }
-            );
-        }
+        Bot.post('statuses/update', {
+                media_ids: new Array(data.media_id_string)
+            }
+        );
     });
 }
 
 upload_image();
-
-
-// var phraseArray = [ "hello darkness",
-//     "my old friend",
-//     "chirp chirp",
-//     "so how bout that weather",
-//     "indescribable",
-//     "delete this neffew",
-//     "itz me snek",
-//     "meirl",
-//     "how would pants wear pants?",
-//     "i aint the sharpest tool in the shed" ];
-//
-// function chooseRandom(myArray) {
-//     return myArray[Math.floor(Math.random() * myArray.length)];
-// }
-//
-// var phrase = chooseRandom(phraseArray) + ", " + chooseRandom(phraseArray);
-// Bot.tweet(phrase);
